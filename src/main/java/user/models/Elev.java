e28d6fbdcaef82861f6565bdfdbcecd202abd0d4
Ruben <174733720+ruben-23@users.noreply.github.com>
2024-11-25 21:12:40 +0200

    Adaugare clasa Elev

---
+++ src/main/java/user/models/Elev.java
@@ -0,0 +1,13 @@
+package user.models;
+
+public class Elev extends Persoana {
+
+    public Elev(String nume, String prenume, String adresa, String numarTelefon, String CNP) {
+        super(nume, prenume, adresa, numarTelefon, CNP);
+    }
+
+    public void login(){}
+    public void logout(){}
+
+
+}
