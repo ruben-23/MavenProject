31d27f962466b650bf75e41a2ddf7ce72edc71ac
Ruben <174733720+ruben-23@users.noreply.github.com>
2024-11-26 14:19:14 +0200

    Implementare metode pentru crearea si inchiderea conexiunii

--- src/main/java/database/ConectareDB.java
+++ src/main/java/database/ConectareDB.java
@@ -1,4 +1,44 @@
 package database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConectareDB {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/liceu_db";
+    private static final String USER = "root";
+    private static final String PAROLA = "password";
+
+    private static Connection conexiune;
+
+    private ConectareDB() {}
+
+    public Connection getConexiune() {
+
+        if(conexiune == null){
+            try {
+                conexiune = DriverManager.getConnection(URL, USER, PAROLA);
+            } catch(SQLException e) {
+                e.printStackTrace();
+                throw new RuntimeException("Conectarea la baza de date a esuat", e);
+            }
+        }
+
+        return conexiune;
+    }
+
+    public static void closeConexiune(){
+
+        if(conexiune != null){
+            try{
+                conexiune.close();
+                conexiune = null;
+            } catch(SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
 }
